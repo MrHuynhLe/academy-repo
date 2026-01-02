ea96a52e4950ba6db08d2a39acbd520840e225f3
huynhlvph36158 <huynhlvph36158@fpt.edu.vn>
2026-01-02 23:26:35 +0700

    base

---
+++ E-class/studen-management-backend/src/main/java/com/vn/studenmanagementbackend/StudenManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vn.studenmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudenManagementBackendApplication.class, args);
+    }
+
+}
---
+++ E-class/studen-management-backend/src/test/java/com/vn/studenmanagementbackend/StudenManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.studenmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
